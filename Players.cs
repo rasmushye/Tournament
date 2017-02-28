044b7bd94c26da8871fc0be87834602b2b2099ee
rasmushye <rasmus@hyes.dk>
2017-02-28 11:56:05 +0100

    Add files via upload

---
+++ Players.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament
+{
+    public class Spiller
+    {
+        public enum GenderType { Male, Female};
+
+        private String FirstName { get; set; }
+        private String LastName { get; set; }
+        private int Age { get; set; }
+        private GenderType Gender { get; set; }
+
+        public Spiller(string fn, string ln, int age, GenderType g)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Age = age;
+            Gender = g;
+        }
+
+        public override string ToString()
+        {
+            var rv = FirstName + " " + LastName + " " + Age + " " + Gender;
+            return rv;
+        }
+
+        static void Main(string[] args)
+        {
+            var smissel = new Spiller("Niklas", "Missel", 22, GenderType.Male);
+            Console.WriteLine(smissel);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wimbledon
+{
+    class Tournament
+    {
+        private String Name { get; set; }
+        private DateTime Year { get; set; }
+        private DateTime FromDate { get; set; }
+        private DateTime ToDate { get; set; }
+        private List<string> PlayerList= new List<string>();
+        private int numberPlayers;
+        private int Match { get; set; }
+
+        public Tournament(string name, DateTime year, DateTime fd, DateTime td, int match, int numberPlayers)
+        {
+            Name = name;
+            Year = year;
+            FromDate = fd;
+            ToDate = td;
+            Match = match;
+            numberPlayers = 64;
+
+
+        }
+/*
+        public void addPlayer(int player)
+        {
+            if(PlayerList.Count == numberPlayers)
+            {
+                return;
+            }
+            else
+            {
+                Player.Add(player);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    */
+    }
+    
+    
+}
+
